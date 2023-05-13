2391ef6e4a26c0353a6387d6b77395b5023d9125
aibrahimkcc <aibrahimkcc@gmail.com>
2023-05-13 03:01:03 +0300

    ilk commitw

---
+++ ekip.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/b8e73f98a0.js" crossorigin="anonymous"></script>
+    <title>Anasayfa</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <section id="menu">
+
+    <div id="logo">Avukat</div>
+    <nav>
+        <a href=""><i class="fa-sharp fa-solid fa-house ikon"></i>Anasayfa</a>
+        <a href=""><i class="fa-solid fa-info ikon"></i>Hakkımızda</a>
+        <a href=""><i class="fa-solid fa-book-open ikon"></i>Yazılar</a>
+        <a href=""><i class="fa-sharp fa-solid fa-people-group ikon"></i>Ekip</a>
+        <a href=""><i class="fa-sharp fa-solid fa-phone ikon"></i>İletişim</a>
+    </nav>
+    </section>
+
+    <section id="anasayfa">
+        <div id="black"> 
+        </div>
+        <div id="icerik">
+            <h2>Avukat göt deliği</h2>
+            <hr width= 300 align="left">
+            <p>Lorem ipsum dolor, sit amet consecte
+                tur adipisicing elit. Rem saepe quo a
+                spernatur adipisci totam temporibus in consectet
+                ur eveniet, fugiat vero et repellendus necessitatibus. 
+                Qui assumenda pariatur voluptas! Quaerat, eligendi i
+                ncidunt?
+            </p>
+        </div>
+    </section>
+    <section id="ekip">
+        <div class="container">
+            <h3 id="ekiph3">Ekip</h3>
+
+            <div class="sutun-orta">
+                <img src="img/ekip1.jpg" alt=""
+                 class="img-fluid oval">
+                <h4 class="ekipismi">Avukat Göt Deliği</h4>
+                <p class="ekipp">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, eligendi.
+
+                </p>
+                <div class="ekip-ikon">
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                </div>
+            </div>
+
+
+            <div class="sutun-sol-sag">
+                <img src="img/ekip2.jpg" alt=""
+                 class="img-fluid oval">
+                <h4 class="ekipismi">Avukat burdur</h4>
+                <p class="ekipp">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, eligendi.
+
+                </p>
+                <div class="ekip-ikon">
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                </div>
+            
+            </div>
+
+            <div class="sutun-sol-sag">
+                <img src="img/ekip3.jpg" alt=""
+                 class="img-fluid oval">
+                <h4 class="ekipismi">Avukat Göt Deliği</h4>
+                <p class="ekipp">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, eligendi.
+
+                </p>
+                <div class="ekip-ikon">
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                </div>
+            </div>
+        
+        </div>
+
+
+    </section>
+
+    <section id="iletisim">
+        <div class="container">
+            <h3 id="h3iletisim">İletişim</h3>
+
+            <form action="index.php" method="post">
+            <div id="iletisimopak">
+                <div id="formgroup">
+                    <div id="solform">
+                        <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+                        <input type="text" name="tel" placeholder="Telefon" required class="form-control">
+                    </div>
+            
+                    <div id="sagform">
+                        <input type="email" name="mail" placeholder="Email adresiniz" required class="form-control">
+                        <input type="text" name="konu" placeholder="Konu Başlığı" required class="form-control">
+                    </div>
+                    <textarea name="mesaj" id="" cols="80"  rows="10" >
+                    </textarea>
+                    <br>
+                    <input type="submit" value="Gönder">
+                </div>
+                <div id="adres">
+                    <h4 id="adresh4">Adres :</h4>
+                    <p class="adresp">Mehmet Mah</p>
+                    <p class="adresp">Akyılduz cad</p>
+                    <p class="adresp">oğuz sokak no:11</p>
+                    <p class="adresp">0212 123 12 12</p>
+                    <p class="adresp">Email:anan@anan.com</p>
+
+                </div>
+            </div>
+            </form>
+            
+            <footer>
+                <div id="copyright">2020 Tüm hakları saklıdır
+                </div>
+
+                <div> 
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                    <a href="#"><i class="fa-brands fa-square-twitter social"></i> </a>
+                </div>
+                <a href="#menu"><i class="fa-solid fa-chevron-up"></i></a>
+            </footer>
+        </div>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js" integrity="sha256-a2yjHM4jnF9f54xUQakjZGaqYs/V1CYvWpoqZzC2/Bw=" crossorigin="anonymous"></script>
+    <script src="owl/owl.carousel.min.js"></script>
+    <script src="owl/script.js"></script>
+</body>
+</html>
+<?php include ('baglanti.php'); 
+if (isset($_POST["isim"], $_POST["tel"], $_POST["mail"] , $_POST["konu"] , $_POST["mesaj"] ))
+{
+    $adsoyad=$_POST["isim"];
+    $telefon=$_POST["tel"];
+    $email=$_POST["mail"];
+    $konu=$_POST["konu"];
+    $mesaj=$_POST["mesaj"];
+    
+    $ekle="INSERT INTO iletisim (adsoyad, telefon, email, konu, mesaj)
+     VALUES ('".$adsoyad."','".$telefon."','".$email."','".$konu."','".$mesaj."')";
+
+     if($baglan->query($ekle)===TRUE)
+    
+     {
+        echo "<script>alert('Mesajınız gönderildi.')</script>";
+    }
+
+    else{
+        echo"<script>alert('Mesaj gönderilirken bir hata oluştu.')</script>";
+    }
+
+}
+?>
